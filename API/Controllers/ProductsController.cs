9d073c8ca668be49d47cea52b8eb7b67c96855b4
Hin <nth251120@gmail.com>
2021-07-25 17:55:37 +0700

    Add simple API

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    
+    public class ProductsController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public ProductsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            return  await _context.Products.ToListAsync();
+            
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+    }
+}
